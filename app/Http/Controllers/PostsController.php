a800499e66219a8f082b466492276336838b6a7c
Marco Tello  <muchahuev@hotmail.com>
2020-01-29 16:54:48 -0600

    adding the DB connection and the Post model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,22 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Post;
 
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first post!',
-            'my-second-post' => 'This is a second post.',
-        ];
+        // This is a normal query thatwe can execute 
+        // $post = DB::table('posts')->where('slug', $slug)->first();
 
-        if(! array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
-        }
+        // This is an eloquent query that PHP is going to form and execute.
+        $post = Post::where('slug', $slug)->firstOrFail();
+        
+        // this is a qick way to inspect a variable in the browser and see what it contains.
+        // dd($post);
         
         return view('post', [
-            'post' => $posts[$post]
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
